a277e3c5f6aae29f121977429c15c4b1d9acf6ba
ceo <john@talisol.com>
2022-09-18 20:39:30 +0900

    fix coding mistakes

--- create_bunka_db.py
+++ create_bunka_db.py
@@ -15,23 +15,27 @@ kata_tupple = ('ア','イ','ウ','エ','オ','カ','キ','ク','ケ','コ','サ'
 df = pd.read_csv("./bunka.csv")
 
 
-def populateDict(dict_,base_pronunciation,examples_line_break_down,pronunciation_type):
+def populateDict(dict_,most_common_word_pronunciation,example_list,pronunciation_type,kanji=None):
     
     populated_dict = dict_
-    example_list = examples_line_break_down[j].split("，")
-    if '' in example_list: example_list.remove('')
+    
+    # example_list = examples_line_break_down[j].split("，")
+    # if '' in example_list: example_list.remove('')
     
     example_dict = dict()
     
+    # if kanji is not None:
+    #     print(kanji,example_list)
 
     for i,example in enumerate(example_list):
   
+
         ind_parenthesis = example.find("（")
         
         if ind_parenthesis != -1:
             example = example[:ind_parenthesis]
 
-        target_furigana = getTargetWordFurigana(example,base_pronunciation)
+        whole_word_furigana = getTargetWordFurigana(example,most_common_word_pronunciation)
         
         
 
@@ -40,16 +44,31 @@ def populateDict(dict_,base_pronunciation,examples_line_break_down,pronunciation
             
             # Given the structure of the data,　in most cases only the first 
             # example matches exactly the pronunciation
+
+
             if i == 0:
                 
-                stem_furigana = removeOkurigana(example,target_furigana)
+                stem_furigana = removeOkurigana(example,whole_word_furigana)
             
-            # print("base:", base_pronunciation,"example:",example,"target_furigana",target_furigana,"stem_furigana",stem_furigana)
+            # print("whole:", whole_word_furigana,"stem:",stem_furigana)
+            
+            if len(whole_word_furigana) == 0:
+                target_furigana = stem_furigana
+                base_pronunciation = stem_furigana
+            elif len(stem_furigana) == 0:
+                target_furigana = whole_word_furigana
+                base_pronunciation = whole_word_furigana
+            else:
+                target_furigana = whole_word_furigana[:len(stem_furigana)]
+                base_pronunciation = stem_furigana
+            # print("base:", base_pronunciation,"example:",example,"whole_word_furigana",whole_word_furigana,"stem_furigana",stem_furigana)
         else:
-            stem_furigana = target_furigana
+            base_pronunciation = most_common_word_pronunciation
+            target_furigana = whole_word_furigana
 
-        example_dict[example] = stem_furigana
+        example_dict[example] = target_furigana
         
+
     populated_dict[pronunciation_type + str(len(dict_) + 1)] = {
         "base_pronunciation": base_pronunciation,
         "examples": example_dict,
@@ -83,21 +102,31 @@ for i,row in df.iterrows():
     on_yomi = dict()
     kun_yomi = dict()  
     
+    if '\xa0 ' in pronun_line_break_down: pronun_line_break_down.remove('\xa0 ')
     
-    
-    for j, base_pronunciation in enumerate(pronun_line_break_down):
+    print(pronun_line_break_down,examples_line_break_down)
+    for j, most_common_word_pronunciation in enumerate(pronun_line_break_down):
         
-        base_pronunciation = base_pronunciation.replace('　','')
+        most_common_word_pronunciation = most_common_word_pronunciation.replace('　','')
+
+        # if '' in examples_line_break_down: examples_line_break_down.remove('')        
+        example_list = examples_line_break_down[j].split("，")
+
+        if example_list == [''] : break
+        
+        if '' in example_list: example_list.remove('')   
+        
+        # print(kanji,examples_line_break_down)
         
         for char in kata_tupple:
-            if char in base_pronunciation:
-                on_yomi = populateDict(on_yomi,base_pronunciation,examples_line_break_down,"on")
+            if char in most_common_word_pronunciation:
+                on_yomi = populateDict(on_yomi,most_common_word_pronunciation,example_list,"on",kanji)
                 break           
             
             
         for char in hira_tupple:
-            if char in base_pronunciation:
-                kun_yomi = populateDict(kun_yomi,base_pronunciation,examples_line_break_down,"kun")
+            if char in most_common_word_pronunciation:
+                kun_yomi = populateDict(kun_yomi,most_common_word_pronunciation,example_list,"kun",kanji)
                 break
 
     kanji_dict[kanji] = {"on": on_yomi, "kun": kun_yomi}
